47782663f71d72a0f96a5eed7e662b6148fd80cb
8891-LONGPT-DES\admin <driverofl01@gmail.com>
2020-12-28 16:12:44 +0700

    add some for cart

--- MyWeb/SimpleWeb/src/main/java/com/company/spring/controller/client/ProductController.java
+++ MyWeb/SimpleWeb/src/main/java/com/company/spring/controller/client/ProductController.java
@@ -1,6 +1,5 @@
 package com.company.spring.controller.client;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.company.spring.entity.Category;
-import com.company.spring.entity.Product;
-import com.company.spring.service.CategoryService;
+import com.company.spring.entity.Product; 
 import com.company.spring.service.ProductService;
 
 @Controller
@@ -19,8 +16,6 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
-	@Autowired
-	private CategoryService categoryService;
 
 	@RequestMapping("/product")
 	public ModelAndView listEmployee(ModelAndView model)  {
--- MyWeb/SimpleWeb/src/main/java/com/company/spring/request/ProductRequest.java
+++ MyWeb/SimpleWeb/src/main/java/com/company/spring/request/ProductRequest.java
@@ -1,9 +1,5 @@
 package com.company.spring.request;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 public class ProductRequest {
 
